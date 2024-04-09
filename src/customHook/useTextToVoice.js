1fc3d5bb43d51fd0167c4452c718fa99654be5fc
Zaman <zamanofficial1980@gmail.com>
2024-04-10 02:26:25 +0530

    committed some changes in my repos

--- src/customHook/useTextToVoice.js
+++ src/customHook/useTextToVoice.js
@@ -33,36 +33,3 @@ const useTextToVoice = () => {
 
 export default useTextToVoice;
 
-//------------------------------------------------------
-
-// import { useState, useEffect } from 'react';
-
-// const synth = window.speechSynthesis;
-
-// const speak = (state, voice) => {
-//   const utterance = new SpeechSynthesisUtterance();
-//   utterance.voice = voice;
-//   utterance.text = state.text;
-//   synth.speak(utterance);
-//   // utterance.rate = state.rate;
-//   // utterance.onend = resolve;
-// };
-
-// const useTextToSpeech = () => {
-//   const [voices, setVoices] = useState([]);
-
-//   useEffect(() => {
-//     const list = synth.getVoices();
-//     setVoices(list);
-//   }, []);
-
-//   return {
-//     voices,
-//     speak: speak,
-//     pause: () => synth.pause(),
-//     resume: () => synth.resume(),
-//     cancel: () => synth.cancel(),
-//   };
-// };
-
-// export default useTextToSpeech;
