90933e1b744fbc6dc62af1c10eeabd28a208c226
Zaman <zamanofficial1980@gmail.com>
2024-04-09 23:46:06 +0530

    committed some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,52 @@
 import React, { useState, useEffect } from 'react'
-// import useTextToVoice from './customHook/useTextToVoice';
+import useTextToVoice from './customHook/useTextToVoice';
 
 function App() {
-  // const [text, setText] = useState("Don't Forget to Subscribe");
-  // const [voice, setVoice] = useState(null);
-  // const { voices, speak, pause, resume, cancel } = useTextToVoice();
+  const [text, setText] = useState("Don't Forget to Subscribe");
+  const [voice, setVoice] = useState(null);
+  const { voices, speak, pause, resume, cancel } = useTextToVoice();
 
-  // useEffect(() => {
-  //   setVoice(voice[0])
-  // },[voices]);
+  useEffect(() => {
+    setVoice(voices[0])
+  },[voices]);
 
-  // const onClick = () =>speak({text}, voice);
+  const onClick = () =>speak({text}, voice);
 
-  // const onVoiceChange =(e) =>{
-  //   setVoice(voices.find((v) => v.name === e.target.value));
-  // };
+  const onVoiceChange =(e) =>{
+    setVoice(voices.find((v) => v.name === e.target.value));
+  };
 
-  // const onTextChange = (e) => {
-  //   setText(e.target.value);
-  // }
+  const onTextChange = (e) => {
+    setText(e.target.value);
+  }
 
   return (
-    // <main className='bg-slate-700'>
-     
-    //   <h1 className='text-red'>Text To Audio</h1>
-    //   <textarea rows="8" cols="72" value={text} onChange={onTextChange} />
-    //   <select value= {voice?.name} onChange={onVoiceChange}>
-    //   <option value={null}>Select Voice</option>
-    //   {voices.map((voice)=>(
-    //       <option key={voice.name} value={voice.name}>{voice.name}</option>
-    //     ))}
-    //   </select>
 
-    //   <Section>
-    //     <button className='bg-red-500' onClick={onClick}>Start</button>
-    //     <button onClick={pause}>Pause</button>
-    //     <button onClick={resume}>Resum</button>
-    //     <button onClick={cancel}>Cancel</button>
-    //   </Section>
-      
-    // </main>
-    <main className='flex justify-content-center items-center m-20 w-1/2  bg-slate-500'>
-      <h1>Text To Audio</h1>
+    
+    <main className='flex flex-wrap justify-center  w-full h-screen bg-slate-700 text-slate-200'>
+      <div className='flex justify-content-center flex-col items-center w-1/2  bg-slate-900 rounded-lg my-20 p-2 text-center'>
+      <h1 className='font-bold text-2xl pb-5'>Text To Audio</h1>
+      <textarea className='bg-slate-400 text-black m-2 p-2  rounded-md'  cols="72" rows="8" value={text} onChange={onTextChange} />
 
+      <select className='w-2/3 m-3 bg-transparent border-2 border-white p-1 rounded-md' value={voice?.name} onChange={onVoiceChange}>
+        <option className="text-black bg-slate-400 border-none"value={null}> Select Voice</option>
+        {voices.map((voice) => (
+          <option className="text-black bg-slate-400 border-none" key={voice.name} value={voice.name}>{voice.name}</option>
+        ))}
+      </select>
+      <section className='flex flex-wrap flex-row gap-3'>
+        <button className='bg-blue-500 px-4 py-0.5 rounded-md font-bold bottom-1 border-black my-3' onClick={onClick}>Start</button>
+        <button className='bg-blue-500 px-4 py-0.5 rounded-md font-bold bottom-1 border-black my-3' onClick={pause}>Pause</button>
+        <button className='bg-blue-500 px-4 py-0.5 rounded-md font-bold bottom-1 border-black my-3' onClick={resume}>Resume</button>
+        <button className='bg-blue-500 px-4 py-0.5 rounded-md font-bold bottom-1 border-black my-3' onClick={cancel}>Cancel</button>
+      </section>
+
+      </div>
     </main>
+  
   );
 }
+
 
 export default App;
 
